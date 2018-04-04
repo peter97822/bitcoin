bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

Remove direct bitcoin calls from qt/optionsdialog.cpp

src/qt/optionsmodel.h
@@ -76,6 +76,8 @@ public:
     void setRestartRequired(bool fRequired);
     bool isRestartRequired();
 
+    ipc::Node& getIpcNode() const { return ipcNode; }
+
 private:
     ipc::Node& ipcNode;
     /* Qt-only settings */

peter97822/bitcoin

src/qt/optionsmodel.h
@@ -79,6 +79,8 @@ public:
     void setRestartRequired(bool fRequired);
     bool isRestartRequired() const;
 
+    interface::Node& node() const { return m_node; }
+
 private:
     interface::Node& m_node;
     /* Qt-only settings */