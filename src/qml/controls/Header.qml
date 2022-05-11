bitcoin-core/gui-qml

qml: update UI elements to use Theme color definitions

src/qml/controls/Header.qml
@@ -28,7 +28,7 @@ Control {
             font.family: "Inter"
             font.styleName: root.bold ? "Semi Bold" : "Regular"
             font.pointSize: root.headerSize
-            color: "#FFFFFF"
+            color: Theme.color.neutral0
             text: root.header
             horizontalAlignment: center ? Text.AlignHCenter : Text.AlignLeft
             wrapMode: Text.WordWrap
@@ -43,7 +43,7 @@ Control {
                 font.family: "Inter"
                 font.styleName: "Regular"
                 font.pointSize: root.descriptionSize
-                color: "#DEDEDE"
+                color: Theme.color.neutral4
                 text: root.description
                 horizontalAlignment: root.center ? Text.AlignHCenter : Text.AlignLeft
                 wrapMode: Text.WordWrap
@@ -59,7 +59,7 @@ Control {
                 font.family: "Inter"
                 font.styleName: "Regular"
                 font.pixelSize: root.subtextSize
-                color: "#FFFFFF"
+                color: Theme.color.neutral0
                 text: root.subtext
                 horizontalAlignment: root.center ? Text.AlignHCenter : Text.AlignLeft
                 wrapMode: Text.WordWrap

src/qml/controls/OptionButton.qml
@@ -15,7 +15,7 @@ Button {
     property alias detail: detail_loader.sourceComponent
     background: Rectangle {
         border.width: 1
-        border.color: button.checked ? "#F7931A" : button.hovered ? "white" : "#999999"
+        border.color: button.checked ? Theme.color.orange : button.hovered ? Theme.color.neutral0 : Theme.color.neutral6
         radius: 10
         color: "transparent"
         Rectangle {
@@ -23,7 +23,7 @@ Button {
             anchors.fill: parent
             anchors.margins: -4
             border.width: 2
-            border.color: "#F7931A"
+            border.color: Theme.color.orange
             radius: 14
             color: "transparent"
             opacity: 0.4
@@ -50,7 +50,7 @@ Button {
                 visible: active
                 sourceComponent: Label {
                     background: Rectangle {
-                        color: "white"
+                        color: Theme.color.neutral0
                         radius: 3
                     }
                     font.styleName: "Regular"
@@ -59,7 +59,7 @@ Button {
                     rightPadding: 7
                     bottomPadding: 4
                     leftPadding: 7
-                    color: "black"
+                    color: Theme.color.neutral9
                     text: qsTr("Recommended")
                 }
             }

src/qml/controls/TextButton.qml
@@ -8,7 +8,7 @@ import QtQuick.Controls 2.15
 Button {
     id: root
     property int textSize: 18
-    property string textColor: "white"
+    property string textColor: Theme.color.neutral0
     font.family: "Inter"
     font.styleName: "Semi Bold"
     font.pointSize: root.textSize

peter97822/bitcoin

src/qml/controls/Header.qml
@@ -28,7 +28,7 @@ Control {
             font.family: "Inter"
             font.styleName: root.bold ? "Semi Bold" : "Regular"
             font.pointSize: root.headerSize
-            color: "#FFFFFF"
+            color: Theme.color.neutral9
             text: root.header
             horizontalAlignment: center ? Text.AlignHCenter : Text.AlignLeft
             wrapMode: Text.WordWrap
@@ -43,7 +43,7 @@ Control {
                 font.family: "Inter"
                 font.styleName: "Regular"
                 font.pointSize: root.descriptionSize
-                color: "#DEDEDE"
+                color: Theme.color.neutral8
                 text: root.description
                 horizontalAlignment: root.center ? Text.AlignHCenter : Text.AlignLeft
                 wrapMode: Text.WordWrap
@@ -59,7 +59,7 @@ Control {
                 font.family: "Inter"
                 font.styleName: "Regular"
                 font.pixelSize: root.subtextSize
-                color: "#FFFFFF"
+                color: Theme.color.neutral9
                 text: root.subtext
                 horizontalAlignment: root.center ? Text.AlignHCenter : Text.AlignLeft
                 wrapMode: Text.WordWrap

src/qml/controls/OptionButton.qml
@@ -15,7 +15,7 @@ Button {
     property alias detail: detail_loader.sourceComponent
     background: Rectangle {
         border.width: 1
-        border.color: button.checked ? "#F7931A" : button.hovered ? "white" : "#999999"
+        border.color: button.checked ? Theme.color.orange : button.hovered ? Theme.color.neutral9 : Theme.color.neutral5
         radius: 10
         color: "transparent"
         Rectangle {
@@ -23,7 +23,7 @@ Button {
             anchors.fill: parent
             anchors.margins: -4
             border.width: 2
-            border.color: "#F7931A"
+            border.color: Theme.color.orange
             radius: 14
             color: "transparent"
             opacity: 0.4
@@ -50,7 +50,7 @@ Button {
                 visible: active
                 sourceComponent: Label {
                     background: Rectangle {
-                        color: "white"
+                        color: Theme.color.neutral9
                         radius: 3
                     }
                     font.styleName: "Regular"
@@ -59,7 +59,7 @@ Button {
                     rightPadding: 7
                     bottomPadding: 4
                     leftPadding: 7
-                    color: "black"
+                    color: Theme.color.neutral0
                     text: qsTr("Recommended")
                 }
             }

src/qml/controls/TextButton.qml
@@ -8,7 +8,7 @@ import QtQuick.Controls 2.15
 Button {
     id: root
     property int textSize: 18
-    property string textColor: "white"
+    property string textColor: Theme.color.neutral9
     font.family: "Inter"
     font.styleName: "Semi Bold"
     font.pointSize: root.textSize