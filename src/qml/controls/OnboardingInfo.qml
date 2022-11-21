jarolrod/gui-qml

qml: conditionally load the banner in the OnboardingInfo control
refactors the banner property in OnboardingInfo from an alias to a bool
which signals when to have the banner_loader as active. Introduces
the bannerItem as the new property which holds the banner as an alias;
updates banner definitions to set bannerItem.

src/qml/controls/OnboardingInfo.qml
@@ -9,8 +9,9 @@ import org.bitcoincore.qt 1.0
 
 Item {
     id: root
-    property alias banner: banner_loader.sourceComponent
+    property alias bannerItem: banner_loader.sourceComponent
     required property string buttonText
+    property bool banner: true
     property bool bold: false
     property bool center: true
     property int bannerMargin: 20
@@ -33,11 +34,11 @@ Item {
         spacing: 0
         Loader {
             id: banner_loader
-            active: true
+            active: root.banner
             visible: active
             Layout.alignment: Qt.AlignCenter
             Layout.topMargin: root.bannerMargin
-            sourceComponent: root.actionItem
+            sourceComponent: root.bannerItem
         }
         Header {
             Layout.fillWidth: true

peter97822/bitcoin

src/qml/controls/OnboardingInfo.qml
@@ -9,8 +9,9 @@ import org.bitcoincore.qt 1.0
 
 Item {
     id: root
-    property alias banner: banner_loader.sourceComponent
+    property alias bannerItem: banner_loader.sourceComponent
     required property string buttonText
+    property bool bannerActive: true
     property bool bold: false
     property bool center: true
     property int bannerMargin: 20
@@ -33,11 +34,11 @@ Item {
         spacing: 0
         Loader {
             id: banner_loader
-            active: true
+            active: root.bannerActive
             visible: active
             Layout.alignment: Qt.AlignCenter
             Layout.topMargin: root.bannerMargin
-            sourceComponent: root.actionItem
+            sourceComponent: root.bannerItem
         }
         Header {
             Layout.fillWidth: true