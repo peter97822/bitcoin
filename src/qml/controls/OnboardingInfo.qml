jarolrod/gui-qml

qml: allow OnboardingInfo to load a detail into its ColumnLayout
This extends the OnboardingInfo control to work for more pages by
allowing it to load a detail into its ColumnLayout. This also allows us
to drop redundant code and AnchorChanges definitions.

src/qml/controls/OnboardingInfo.qml
@@ -10,8 +10,11 @@ import org.bitcoincore.qt 1.0
 Item {
     id: root
     property alias bannerItem: banner_loader.sourceComponent
+    property alias detailItem: detail_loader.sourceComponent
     required property string buttonText
     property bool banner: true
+    property bool detail: false
+    property bool finish: false
     property bool bold: false
     property bool center: true
     property int bannerMargin: 20
@@ -56,6 +59,16 @@ Item {
             subtextMargin: root.subtextMargin
             subtextSize: root.subtextSize
         }
+        Loader {
+            id: detail_loader
+            active: root.detail
+            visible: active
+            Layout.alignment: Qt.AlignCenter
+            Layout.topMargin: 30
+            Layout.leftMargin: 20
+            Layout.rightMargin: 20
+            sourceComponent: root.detailItem
+        }
     }
     ContinueButton {
         id: continueButton
@@ -64,7 +77,7 @@ Item {
         anchors.leftMargin: 20
         anchors.rightMargin: 20
         text: root.buttonText
-        onClicked: swipeView.incrementCurrentIndex()
+        onClicked: root.finish ? swipeView.finished = true : swipeView.incrementCurrentIndex()
     }
 
     state: AppMode.state

peter97822/bitcoin

src/qml/controls/OnboardingInfo.qml
@@ -10,8 +10,11 @@ import org.bitcoincore.qt 1.0
 Item {
     id: root
     property alias bannerItem: banner_loader.sourceComponent
+    property alias detailItem: detail_loader.sourceComponent
     required property string buttonText
     property bool bannerActive: true
+    property bool detailActive: false
+    property bool lastPage: false
     property bool bold: false
     property bool center: true
     property int bannerMargin: 20
@@ -56,6 +59,16 @@ Item {
             subtextMargin: root.subtextMargin
             subtextSize: root.subtextSize
         }
+        Loader {
+            id: detail_loader
+            active: root.detailActive
+            visible: active
+            Layout.alignment: Qt.AlignCenter
+            Layout.topMargin: 30
+            Layout.leftMargin: 20
+            Layout.rightMargin: 20
+            sourceComponent: root.detailItem
+        }
     }
     ContinueButton {
         id: continueButton
@@ -64,7 +77,7 @@ Item {
         anchors.leftMargin: 20
         anchors.rightMargin: 20
         text: root.buttonText
-        onClicked: swipeView.incrementCurrentIndex()
+        onClicked: root.lastPage ? swipeView.finished = true : swipeView.incrementCurrentIndex()
     }
 
     state: AppMode.state