bitcoin-core/gui-qml

qml: Wire Storage amount options to OptionsModel backend
Wires both the easy and advanced configuration options related to
storage amount to the options model backend. Additionally, this removes
an unused and unwanted storage location setting from the advanced
storage amount settings table.

src/qml/components/StorageOptions.qml
@@ -19,11 +19,18 @@ ColumnLayout {
         description: qsTr("Uses about 2GB. For simple wallet use.")
         recommended: true
         checked: true
+        onClicked: {
+            options.prune = true
+            options.pruneSizeGB = 2
+        }
     }
     OptionButton {
         Layout.fillWidth: true
         ButtonGroup.group: group
         text: qsTr("Store all data")
         description: qsTr("Uses about 550GB. Support the network.")
+        onClicked: {
+            options.prune = false
+        }
     }
 }

src/qml/components/StorageSettings.qml
@@ -11,21 +11,18 @@ ColumnLayout {
     spacing: 20
     Setting {
         Layout.fillWidth: true
-        header: qsTr("Store Recent blocks only")
-        actionItem: OptionSwitch {}
-    }
-    Setting {
-        Layout.fillWidth: true
-        header: qsTr("Storage limit")
-        actionItem: ValueInput {
-            description: qsTr("2 GB")
+        header: qsTr("Store recent blocks only")
+        actionItem: OptionSwitch {
+            checked: options.prune
+            onToggled: options.prune = checked
         }
     }
     Setting {
         Layout.fillWidth: true
-        header: qsTr("Data location")
+        header: qsTr("Storage limit (GB)")
         actionItem: ValueInput {
-            description: qsTr("c://.../data")
+            description: options.pruneSizeGB
+            onEditingFinished: options.pruneSizeGB = parseInt(text)
         }
     }
 }

peter97822/bitcoin

src/qml/components/StorageOptions.qml
@@ -19,11 +19,18 @@ ColumnLayout {
         description: qsTr("Uses about 2GB. For simple wallet use.")
         recommended: true
         checked: true
+        onClicked: {
+            optionsModel.prune = true
+            optionsModel.pruneSizeGB = 2
+        }
     }
     OptionButton {
         Layout.fillWidth: true
         ButtonGroup.group: group
         text: qsTr("Store all data")
         description: qsTr("Uses about 550GB. Support the network.")
+        onClicked: {
+            optionsModel.prune = false
+        }
     }
 }

src/qml/components/StorageSettings.qml
@@ -11,21 +11,18 @@ ColumnLayout {
     spacing: 20
     Setting {
         Layout.fillWidth: true
-        header: qsTr("Store Recent blocks only")
-        actionItem: OptionSwitch {}
-    }
-    Setting {
-        Layout.fillWidth: true
-        header: qsTr("Storage limit")
-        actionItem: ValueInput {
-            description: qsTr("2 GB")
+        header: qsTr("Store recent blocks only")
+        actionItem: OptionSwitch {
+            checked: optionsModel.prune
+            onToggled: optionsModel.prune = checked
         }
     }
     Setting {
         Layout.fillWidth: true
-        header: qsTr("Data location")
+        header: qsTr("Storage limit (GB)")
         actionItem: ValueInput {
-            description: "c://.../data"
+            description: optionsModel.pruneSizeGB
+            onEditingFinished: optionsModel.pruneSizeGB = parseInt(text)
         }
     }
 }